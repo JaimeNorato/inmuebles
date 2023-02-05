8c3da82f706616b645dde75fc0b931827f8f55b5
Jaime Norato <jn901113jn@gmail.com>
2023-02-05 15:24:28 -0500

    se agrega versionado de api

--- app/routes/route_v1.py
+++ app/routes/route_v1.py
@@ -1,24 +1,26 @@
 from flask import Flask, jsonify, request, make_response
 from services.property_service import PropertyService
 
-class Route:
+class RouteV1:
+
+    PREFIX_API = '/api/v1'
 
     app = Flask(__name__)
 
-    @app.route('/', methods=['GET'])
+    @app.route(f'{PREFIX_API}/', methods=['GET'])
     def index():
-        return Route.response({'message': 'run server!'})
+        return RouteV1.response({'message': 'run server!'})
 
-    @app.route('/properties', methods=['GET'])
+    @app.route(f'{PREFIX_API}/properties', methods=['GET'])
     def properties():
         property_service = PropertyService()
-        return Route.response(property_service.get_all())
+        return RouteV1.response(property_service.get_all())
 
-    @app.route('/properties', methods=['POST'])
+    @app.route(f'{PREFIX_API}/properties', methods=['POST'])
     def properties_filter():
         data = request.get_json()
         property_service = PropertyService()
-        return Route.response(property_service.filter(data))
+        return RouteV1.response(property_service.filter(data))
     
     #genera la respuesta http con los datos suministrados
     def response(data):
