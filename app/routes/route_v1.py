ba0504297e5ec132a533d5a61060c6c4fef5709f
Jaime Norato <jn901113jn@gmail.com>
2023-02-05 15:31:45 -0500

    se route padre con funcionalidad general de las rutas

--- app/routes/route_v1.py
+++ app/routes/route_v1.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request, make_response
+from routes.route import Route
 from services.property_service import PropertyService
 
-class RouteV1:
+class RouteV1(Route):
 
     PREFIX_API = '/api/v1'
 
@@ -9,30 +10,15 @@ class RouteV1:
 
     @app.route(f'{PREFIX_API}/', methods=['GET'])
     def index():
-        return RouteV1.response({'message': 'run server!'})
+        return Route.response({'message': 'run server!'})
 
     @app.route(f'{PREFIX_API}/properties', methods=['GET'])
     def properties():
         property_service = PropertyService()
-        return RouteV1.response(property_service.get_all())
+        return Route.response(property_service.get_all())
 
     @app.route(f'{PREFIX_API}/properties', methods=['POST'])
     def properties_filter():
         data = request.get_json()
         property_service = PropertyService()
-        return RouteV1.response(property_service.filter(data))
-    
-    #genera la respuesta http con los datos suministrados
-    def response(data):
-        message = 'success'
-        code = 200
-
-        if not data:
-            message = 'Not data found'
-            code = 404
-
-        return make_response(jsonify({'data': data, 'message': message}), code)
-    
-    # retorna el objeto app
-    def get_app(self) -> Flask:
-        return self.app
+        return Route.response(property_service.filter(data))
